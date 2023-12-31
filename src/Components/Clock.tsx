4187bf021559b962706dc9934a3981a06256fa68
Kirill <kirill_ozornin@mail.ru>
2023-12-31 13:10:25 +0200

    clocks v1

---
+++ src/Components/Clock.tsx
@@ -0,0 +1,34 @@
+import {useEffect, useState} from "react";
+
+
+export const Clock = () => {
+
+    //Сохраняем и получаем корректную дату м время в стейт
+    const [time, setTime] = useState(new Date());
+
+    //Обновляем каждую секунду время
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        //Чистим interval после c. unmounts
+        return () => clearInterval(interval);
+
+    }, []);
+
+    //Получаем часы, минуты, секунды из текущего времени
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    //Приводим данные в нужный формат (в строку)
+    const timeDisplay = `${hours}:${minutes}:${seconds}`
+
+    return (
+        <div>
+            <h1>{timeDisplay}</h1>
+        </div>
+    );
+}
+
